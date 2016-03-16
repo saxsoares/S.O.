8cc07d443f6d4275888f098856f6ac53e7b69aba
saxsoares <sax.soares@gmail.com>
2016-03-16 01:24:38 +0000

    updt

--- shell.c
+++ shell.c
@@ -1,1 +1,12 @@
 #include<stdio.h>
+
+fgets(str, MAX_COM, stdin);
+
+while(str[i] != '\n')
+	while(str != ' ') i++
+	while(str != ' ' && str[i] != '\n'){
+		parameters[ip][jp] = str[i];
+		i++; jp++;
+	}
+	jp=0; ip++;
+}
